50f232f98b4c06628da914c7a648ce92f3cf5e3f
YangFong <yangfong2022@gmail.com>
2025-11-05 23:09:51 +0800

    chore: 格式化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import prettierConfig from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+import prettierConfig from "eslint-config-prettier"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -27,6 +27,6 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
